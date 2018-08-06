78f64d53d1ba4396ef7f82ce30329b693d5f0882
zhezhouzz <zorichzorich@gmail.com>
2018-08-06 09:04:14 -0400

    Add openmp support

--- imp_taco/run.sh
+++ imp_taco/run.sh
@@ -2,4 +2,18 @@
 
 # clang++ -std=c++11 -DTACO page_rank.cpp -ltaco && ./a.out && cat y.tns
 
-clang++ -std=c++14 -O3 -D CUR_DEBUG_LEVEL=$1 page_rank.cpp debug/utils_debug.cpp -ltaco && ./a.out && cat x.tns
+CPPFLAGS=""
+CPPFLAGS+=" -std=c++14 "
+CPPFLAGS+=" -O3 "
+CPPFLAGS+=" -D CUR_DEBUG_LEVEL="$1" "
+CPPFLAGS+=" -ltaco "
+
+#if use openmp
+if [ "$2" = "openmp" ]; then
+  echo "enable openmp"
+  CPPFLAGS+="-fopenmp -L/usr/local/opt/libomp/lib -I/usr/local/opt/libomp/include  -Xpreprocessor -fopenmp -lomp"
+fi
+
+echo $CPPFLAGS
+
+clang++ $CPPFLAGS page_rank.cpp debug/utils_debug.cpp && ./a.out && cat x.tns
